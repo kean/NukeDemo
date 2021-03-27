2204154273899aff37b145fcc9324c1b5f52dd29
kean <grebenyuk.alexander@gmail.com>
2021-03-26 22:32:35 -0400

    Remove redundant available check

--- Sources/MenuViewController.swift
+++ Sources/MenuViewController.swift
@@ -13,10 +13,8 @@ final class MenuViewController: UITableViewController {
 
         UIApplication.shared.keyWindow?.tintColor = UIColor(red: 0.992, green: 0.243, blue: 0.416, alpha: 1.00)
 
-        if #available(iOS 11.0, *) {
-            navigationController?.navigationBar.prefersLargeTitles = true
-            navigationItem.largeTitleDisplayMode = .automatic
-        }
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.largeTitleDisplayMode = .automatic
 
         sections = [generalSection, integrationSection, advancedSection]
     }
