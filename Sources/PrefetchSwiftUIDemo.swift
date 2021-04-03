c0727122aebbaf88f72a8a22d769d80e5eff0aa5
kean <grebenyuk.alexander@gmail.com>
2021-04-03 15:23:23 -0400

    Add prefetcher demo for SwiftUI

--- Sources/PrefetchSwiftUIDemo.swift
+++ Sources/PrefetchSwiftUIDemo.swift
@@ -5,25 +5,33 @@
 import Foundation
 import SwiftUI
 import FetchImage
+import Nuke
+
+// MARK: - View
 
 @available(iOS 14.0, *)
 struct PrefetchDemoView: View {
+    @StateObject var model = PrefetchDemoViewModel()
+
     var body: some View {
         GeometryReader { geometry in
             ScrollView {
                 let side = geometry.size.width / 4
                 let item = GridItem(.fixed(side), spacing: 2)
                 LazyVGrid(columns: Array(repeating: item, count: 4), spacing: 2) {
-                    ForEach(demoPhotosURLs.indices) {
-                        ImageView(url: demoPhotosURLs[$0])
+                    ForEach(demoPhotosURLs.indices) { index in
+                        ImageView(url: demoPhotosURLs[index])
                             .frame(width: side, height: side)
                             .clipped()
+                            .onAppear { model.onAppear(index) }
+                            .onDisappear { model.onDisappear(index) }
                     }
                 }
             }
         }
     }
 }
+
 
 @available(iOS 14.0, *)
 struct ImageView: View {
@@ -41,5 +49,43 @@ struct ImageView: View {
         }
         .onAppear { image.load(url) }
         .onDisappear(perform: image.reset)
+    }
+}
+
+// MARK: - ViewModel
+
+final class PrefetchDemoViewModel: ObservableObject, ScrollViewPrefetcherDelegate {
+    private let imagePrefetcher: ImagePrefetcher
+    private let scrollViewPrefetcer: ScrollViewPrefetcher
+    let urls: [URL]
+
+    init() {
+        self.imagePrefetcher = ImagePrefetcher()
+        self.scrollViewPrefetcer = ScrollViewPrefetcher()
+        self.urls = demoPhotosURLs
+
+        self.scrollViewPrefetcer.delegate = self
+    }
+
+    func onAppear(_ index: Int) {
+        scrollViewPrefetcer.onAppear(index)
+    }
+
+    func onDisappear(_ index: Int) {
+        scrollViewPrefetcer.onDisappear(index)
+    }
+
+    // MARK: ScrollViewPrefetcherDelegate
+
+    func getAllIndicesForPefetcher(_ prefetcher: ScrollViewPrefetcher) -> Range<Int> {
+        urls.indices
+    }
+
+    func prefetcher(_ prefetcher: ScrollViewPrefetcher, prefetchItemsAt indices: [Int]) {
+        imagePrefetcher.startPrefetching(with: indices.map { urls[$0] })
+    }
+
+    func prefetcher(_ prefetcher: ScrollViewPrefetcher, cancelPrefechingForItemAt indices: [Int]) {
+        imagePrefetcher.stopPrefetching(with: indices.map { urls[$0] })
     }
 }
